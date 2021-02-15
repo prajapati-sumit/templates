3b383a1a43ea98db2afff3812cf4cb4fa78442bb
prajapati-sumit <72280760+prajapati-sumit@users.noreply.github.com>
2021-02-15 23:18:08 +0530

    Create Python_Template.py

---
+++ Python_Template.py
@@ -0,0 +1,39 @@
+# CODED BY SUMIT KUMAR PRAJAPATI
+import os
+import sys
+import math
+
+MD=int(1e9+7)
+MX=int(1e5+2)
+pi=3.1415926535897932
+
+
+def intinp():
+    return int(input())
+
+def arinput():
+    return list(map(int,input().strip().split()))
+
+
+
+
+def solve():
+    ${1}
+
+
+
+def iofile():
+    sys.stdin=open("input.txt",'r')
+    sys.stdout=open("output.txt",'w')
+
+
+
+if __name__=='__main__':
+
+    iofile()
+    tc=1
+    tc=intinp()
+    for t in range(1,tc+1):
+        # print(f"Case #{t}: ",end='')
+        solve()
+
