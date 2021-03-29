33b0e6fea30603550beba4d033c37341c7a906c4
Sumit Prajapati <72280760+prajapati-sumit@users.noreply.github.com>
2021-03-30 01:34:03 +0530

    Update CPP_Template.cpp

--- CPP_Template.cpp
+++ CPP_Template.cpp
@@ -1,5 +1,5 @@
-//CODED BY SUMIT KUMAR PRAJAPATI
-#include <bits/stdc++.h>
+// AUTHOR: SUMIT PRAJAPATI      
+#include "bits/stdc++.h"
 using namespace std;
 
 typedef unsigned long long ull;
@@ -12,17 +12,20 @@ typedef pair<ll, ll>  pl;
 #define mk make_pair
 #define ff first
 #define ss second
+#define trav(v) for(auto &el:v)
+#define watch(x) cerr<<#x<<" = "<<x<<'\n'
 #define rep(i,n) for(ll i=0;i<n;i++)
 #define repe(i,n) for(ll i=1;i<=n;i++)
 #define FOR(i,a,b) for(ll i=a;i<=b;i++)
 #define printar(a,s,e) FOR(i,s,e)cout<<a[i]<<" ";cout<<'\n'
 #define curtime chrono::high_resolution_clock::now()
 #define timedif(start,end) chrono::duration_cast<chrono::nanoseconds>(end - start).count()
+#define TESTCASES 1
 
 const int INF=1e9;
 const int MX=1e5+5;
 const int MD=1e9+7;
-const int MDL=99824453;
+const int MDL=998244353;
 auto time0 = curtime;
  
  
@@ -32,33 +35,27 @@ auto time0 = curtime;
  
 void solve(){
   
-        
+    $0    
   
  
 } 
  
  
 int main() {
-
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #else
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     srand(time(0)); 
     time0 = curtime;
 
     ll t=1;
-    cin>>t;
+    if(TESTCASES)cin>>t;
     repe(tt,t){
         //cout<<"Case #"<<tt<<": ";
         solve();
     }
     
-    cerr<<"Execution Time: "<<timedif(time0,curtime)*1e-9<<" sec\n";
+    //cerr<<"Execution Time: "<<timedif(time0,curtime)*1e-9<<" sec\n";
     return 0;
  
 }
